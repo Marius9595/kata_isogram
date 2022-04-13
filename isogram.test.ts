37d24fc5301af894a4d706c2ba3f3520cdf1863c
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-13 13:50:19 +0100

    🔴 isogram is a word without repeated letters

--- isogram.test.ts
+++ isogram.test.ts
@@ -8,4 +8,10 @@ describe("isogram", ()=>{
     test("the simplest is an empty word",()=>{
         expect(isogram("")).toBeTruthy();
     })
+
+    test("is a word without repeated letters",()=>{
+        expect(isogram("aaa")).toBeFalsy();
+        expect(isogram("sdaffw")).toBeFalsy();
+        expect(isogram("qwedsfq")).toBeFalsy();
+    })
 })
