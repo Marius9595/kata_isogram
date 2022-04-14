ab3ec7260f9b5e6558058ccb69db466bb7480f17
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-14 23:39:10 +0100

    fix 🔴 isogram is a word without repeated letters consecutives

--- isogram.test.ts
+++ isogram.test.ts
@@ -1,17 +1,21 @@
 function isogram(word:string): boolean {
-    return true;
+    if(word === ""){
+        return true;
+    }
+    return !(word[0] === word[1]);
+
 }
 
 //🟢🔴
 
-describe("isogram", ()=>{
+describe("isogram is a word without repeated letters", ()=>{
     test("the simplest is an empty word",()=>{
         expect(isogram("")).toBeTruthy();
     })
 
-    test("is a word without repeated letters",()=>{
-        expect(isogram("aaa")).toBeFalsy();
-        expect(isogram("sdaffw")).toBeFalsy();
-        expect(isogram("qwedsfq")).toBeFalsy();
+    test("consecutives",()=>{
+        expect(isogram("aalgfgr")).toBeFalsy();
+        expect(isogram("aerww")).toBeFalsy();
+        expect(isogram("aerffew")).toBeFalsy();
     })
 })
